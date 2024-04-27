9f221c453121975ed9aceba2ea3041929cd345e2
srinivasan shanmugam <vasan1983sri@gmail.com>
2024-04-27 09:08:39 -0400

    first commit

---
+++ src/main/java/com/sap/sap_backend/SapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sap.sap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sap/sap_backend/SapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.sap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
